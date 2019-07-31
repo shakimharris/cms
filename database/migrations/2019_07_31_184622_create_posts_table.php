fad84262802e7e3da39d8b8a0c4b541a066f2baf
Shakim Harris <sharris@gls.us.com>
2019-07-31 14:53:28 -0400

    null func

--- database/migrations/2019_07_31_184622_create_posts_table.php
+++ database/migrations/2019_07_31_184622_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('description');
             $table->text('content');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
