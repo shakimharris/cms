45ff9d1871ba4928bcf8b252ed46210a0b0f9f68
Shakim Harris <sharris@gls.us.com>
2019-07-31 14:52:28 -0400

    added columns

--- database/migrations/2019_07_31_184622_create_posts_table.php
+++ database/migrations/2019_07_31_184622_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
             $table->timestamps();
         });
     }
